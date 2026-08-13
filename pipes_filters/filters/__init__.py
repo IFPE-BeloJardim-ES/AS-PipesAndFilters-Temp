bc4601b44b8ad2ff4a4b40d3dc32aa25410831dc
Wiik12223 <buchupedro@gmail.com>
2026-08-13 13:13:59 -0300

    Adicionado duas das atividades opcionais: MaskCPF e removeacent

--- pipes_filters/filters/__init__.py
+++ pipes_filters/filters/__init__.py
@@ -9,10 +9,14 @@ from pipes_filters.filters.uppercase_filter import UppercaseFilter
 from pipes_filters.filters.reverse_filter import ReverseFilter
 from pipes_filters.filters.trim_filter import TrimFilter
 from pipes_filters.filters.replace_filter import ReplaceFilter
+from pipes_filters.filters.maskcpf_filter import MaskCPFFilter
+from pipes_filters.filters.removeacent_filter import RemoveAcentFilter
 
 __all__ = [
     "UppercaseFilter", 
     "ReverseFilter",
     "TrimFilter",
-    "ReplaceFilter"
+    "ReplaceFilter",
+    "MaskCPFFilter",
+    "RemoveAcentFilter"
     ]
--- pipes_filters/main.py
+++ pipes_filters/main.py
@@ -6,13 +6,14 @@ propriedade que a arquitetura Pipes and Filters entrega.
 """
 
 from pipes_filters.pipe import Pipe
-from pipes_filters.filters import UppercaseFilter, ReverseFilter, TrimFilter, ReplaceFilter
-
+from pipes_filters.filters import UppercaseFilter, ReverseFilter, TrimFilter, ReplaceFilter, MaskCPFFilter, RemoveAcentFilter
 
 def main() -> None:
     entrada = "Arquitetura de Software"
     entrada2 = "   O   cliente João  da  Silva,  CPF 123.456.789-00  "
     entrada3 = "   O   cliente João  da  Silva,  CPF 123.456.789-00  "
+    entrada4 = "123.456.789-00"
+    entrada5 = "Olá, meu nome é João Olíveira e estou indo para São Paulo."
 
     pipeline = (
         Pipe()
@@ -30,10 +31,20 @@ def main() -> None:
         .add(ReplaceFilter("Silva", "Souza"))
         .add(TrimFilter())
     )
+    pipeline4 = (
+        Pipe()
+        .add(MaskCPFFilter())
+    )
+    pipeline5 = (
+        Pipe()
+        .add(RemoveAcentFilter())
+    )
 
     saida = pipeline.run(entrada)
     saida2 = pipeline2.run(entrada2)
     saida3 = pipeline3.run(entrada3)
+    saida4 = pipeline4.run(entrada4)
+    saida5 = pipeline5.run(entrada5)
 
     print("Pipeline montado :", pipeline)
     print("Entrada          :", repr(entrada))
@@ -44,6 +55,13 @@ def main() -> None:
     print("\nPipeline montado :", pipeline3)
     print("Entrada 3        :", repr(entrada3))
     print("Saida 3          :", repr(saida3))
+    print("\nPipeline montado :", pipeline4, "(Opcional)")
+    print("Entrada 4        :", repr(entrada4))
+    print("Saida 4          :", repr(saida4))
+    print("\nPipeline montado :", pipeline5, "(Opcional)")
+    print("Entrada 5        :", repr(entrada5))
+    print("Saida 5          :", repr(saida5))
+
 
 
 if __name__ == "__main__":
